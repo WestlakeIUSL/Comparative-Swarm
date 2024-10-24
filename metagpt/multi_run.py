506ca99721d91f6b734d0640691d32ea00494a54
Miang Chen <2594416505@qq.com>
2024-10-24 16:30:59 +0800

    更改相对路径

--- metagpt/multi_run.py
+++ metagpt/multi_run.py
@@ -3,7 +3,7 @@ import subprocess
 def run_app_multiple_times(times: int):
     for i in range(times):
         print(f"Running the app for the {i + 1} time...")
-        subprocess.run(["python", "metagpt/software_company.py"])  # 替换为你的 app 所在的脚本路径
+        subprocess.run(["python", "./software_company.py"])  # 替换为你的 app 所在的脚本路径
         print(f"Finished run {i + 1}")
 
 if __name__ == "__main__":
